01b52b7dd6ce46da84c327dcd67a7e93ee42f88b
Drekstord <113056373+Drekstord@users.noreply.github.com>
2025-04-01 20:25:31 +0200

    exo 1

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-#Simple Math
 
-x = 2
-y = 3
-z = x + y
 
-print(x)
-print(y)
+def compareFunc (x, y);
+    if x > y:
+        return True
+    else:
+        return False
 
-a = 10
-b = 5
-c = a / b
-d = a * b
-e = a % b
-
-name = "Matteo"
-surname = "MatrakMan"
-
-student_name = name + " " + surname
-print(student_name)
+print(compareFunc(2, 3))
