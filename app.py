e4bdb899e17054027001d8f7f1a92e07e9f39a68
Drekstord <113056373+Drekstord@users.noreply.github.com>
2025-04-01 19:41:05 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+#Simple Math
+
+x = 2
+y = 3
+z = x + y
+
+print(x)
+print(y)
+
+a = 10
+b = 5
+c = a / b
+d = a * b
+e = a % b
+
+name = "Matteo"
+surname = "MatrakMan"
+
+student_name = name + " " + surname
+print(student_name)
